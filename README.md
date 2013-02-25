c4a4b13d142684b8f439709e49c19f321a952782
Matthew Teece <mteece@gmail.com>
2013-02-25 09:52:30 -0500

    Initial commit and README update.

--- README.md
+++ README.md
@@ -1,4 +1,40 @@
 Glance
 ======
 
-Lightweight local storage engine prototype.
+Lightweight local storage engine prototype. Uses local storage, serialization. Stores JSON objects.
+
+Getting Started
+---------------
+
+* Dependencies on Modernizer, randomUUID, localStorage.
+
+Need some support to abstract it. It is currently rough and still tied to its one time use. Basically make it more flexible. Add Grunt JS, builds, tests, etc. Like to make it function independent of Modernizer and Underscore. Any help is appreciated.
+
+Conceptual
+----------
+
+Look at https://github.com/LiosK/UUID.js for UUID
+
+	interface Storage {
+		readonly attribute unsigned long length;
+		[IndexGetter] DOMString key(in unsigned long index);
+		[NameGetter] DOMString getItem(in DOMString key);
+		[NameSetter] void setItem(in DOMString key, in DOMString data);
+		[NameDeleter] void removeItem(in DOMString key);
+		void clear();
+	};
+
+Logic is store JSON objects. Serialize in and out. Grab Underscore. Create a catalog object var catalog = {}. Add things by unique id.
+
+	console.log(this.entry());
+	console.log(this.entry('abc', 'the title', 'the description'));
+
+
+Data looks like.
+
+	catalog = {
+		'entries': [
+			{pk: "7384D2E2-D2D4-4440-BC0A-D1C83A0DCA5E", title: "", description: ""}
+			{pk: "7384D2E2-D2D4-4440-BC0A-D1C83A0DCA5E", title: "", description: ""}
+		]
+	}
---
+++ glance.js
@@ -0,0 +1,97 @@
+// Namespace for Glance.
+var Glance = Glance || {};
+
+(function() {
+
+	Glance.init = function() {
+		//debugger;
+		if (Modernizr.localstorage) {
+			// window.localStorage is available!
+			console.log('YES');
+			this.database();
+			return true;
+		} else {
+			console.log('NO');
+			// no native support for HTML5 storage :(
+			// maybe try dojox.storage or a third-party solution
+			return false;
+		}
+	};
+
+	Glance.catalog = {
+		'entries': []
+	};
+
+	Glance.entry = function(t, d) {
+
+		var obj = {};
+		obj.pk = randomUUID();
+		obj.title = t || '';
+		obj.description = d || '';
+
+		return obj;
+	};
+
+	Glance.database = function(){
+		//debugger;
+
+		// Retrieve the object from storage
+		var retrievedObject = localStorage.getItem('catalog');
+		if(retrievedObject) {
+			// We have a catalog.
+			this.catalog = JSON.parse(retrievedObject);
+		} else {
+			// We don't have a catalog Put the object into storage.
+			localStorage.setItem('catalog', JSON.stringify(this.catalog));
+		}
+		console.log(this.catalog);
+	};
+
+	Glance.clear = function() {
+		// debugger;
+		if(this.catalog) {
+			// Flush it.
+			this.catalog.entries = [];
+			// Sync the changes with localstorage.
+			localStorage.setItem('catalog', JSON.stringify(this.catalog));
+		}
+	};
+
+	Glance.save = function(entry) {
+		if(entry && this.catalog) {
+
+		}
+	};
+
+	Glance.add = function(entry) {
+		// debugger;
+		if(entry && this.catalog) {
+			// Push it to the end of the array.
+			this.catalog.entries.push(entry);
+			// Sync the localStorage.
+			localStorage.setItem('catalog', JSON.stringify(this.catalog));
+		}
+	};
+
+	// Returns the first value that matches all of the key-value pairs
+	// Glance.find({pk: "7384D2E2-D2D4-4440-BC0A-D1C83A0DCA5E"});
+ 	// Glance.find({pk: "7384D2E2-D2D4-4440-BC0A-D1C83A0DCA5E", title: "ABC"});
+ 	Glance.find = function(props) {
+ 		var retrievedObject = {};
+		if(props && this.catalog){
+			retrievedObject = _.findWhere(this.catalog.entries, props);
+		}
+		return retrievedObject;
+	};
+
+	Glance.findAll = function() {
+		var retrievedObjects = [];
+		if(this.catalog) {
+			return this.catalog.entries;
+		}
+		return retrievedObjects;
+	};
+
+	Glance.init();
+
+}());
